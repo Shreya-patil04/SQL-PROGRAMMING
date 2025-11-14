19f2f6281d4a46f42edf4aa0f9b12445f9d81a09
Shreya-patil04 <patilshreya0405@gmail.com>
2025-11-14 17:25:41 +0530

    Add employee records to the database
    Inserted multiple employee records into the employees table.

---
+++ 02_insert_operations.sql
@@ -0,0 +1,8 @@
+USE company_db;
+
+-- INSERT RECORDS
+INSERT INTO employees (emp_name, department, salary, hire_date) VALUES
+('Amit Sharma', 'Engineering', 65000, '2024-01-10'),
+('Priya Nair', 'HR', 45000, '2023-12-01'),
+('Rohan Patil', 'Sales', 55000, '2024-02-15'),
+('Sneha Kulkarni', 'Engineering', 70000, '2023-11-11');
