395875a36152678f48fb1cd16b6f00da8b65d23e
Shreya-patil04 <patilshreya0405@gmail.com>
2026-01-03 22:05:39 +0530

    Add examples for ISDATE and CAST functions; include use cases for date validation and conversion

--- INTERMEDIATE LEVEL/10_date_and_time_function_overview.sql
+++ INTERMEDIATE LEVEL/10_date_and_time_function_overview.sql
@@ -219,8 +219,18 @@ ISDATE('20-08-2025') Datecheck3,
 ISDATE('2025') Datecheck4,
 ISDATE('08') Datecheck5
 
-SELECT '2025-0S-20' AS OrderDate UNION
 SELECT 
-
+	--CAST(OrderDate AS DATE) OrderDate
+	OrderDate,
+	ISDATE(OrderDate),
+	CASE WHEN ISDATE(OrderDate) = 1 THEN CAST(OrderDate AS DATE)
+	END NewOrderDate
+FROM (
+SELECT '2025-08-20' AS OrderDate UNION
+SELECT '2025-08-21' UNION
+SELECT '2025-08-23' UNION
+SELECT '2025-08'
+) t
+-- WHERE ISDATE(OrderDate) = 0
 
 
