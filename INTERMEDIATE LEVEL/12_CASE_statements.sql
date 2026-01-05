0409635a740203a10637bdc194e39375ad5a4b05
Shreya-patil04 <patilshreya0405@gmail.com>
2026-01-06 01:40:53 +0530

    Add SQL examples for data mapping and handling NULLs; include use cases for gender display and average score calculation

--- INTERMEDIATE LEVEL/12_CASE_statements.sql
+++ INTERMEDIATE LEVEL/12_CASE_statements.sql
@@ -25,3 +25,75 @@ FROM(
 GROUP BY Category
 ORDER BY total_sales DESC
 
+
+-- 2.MAPPING DATA: -
+-- SQL TASK: Retrieve employee details with gender displayed as full text
+SELECT
+EmployeeID,
+FirstName,
+LastName,
+Gender,
+CASE
+	WHEN Gender = 'F' THEN 'Female'
+	WHEN Gender = 'M' THEN 'Male'
+	ELSE 'Not available'
+END GenderFullText
+FROM Sales.Employees
+
+-- SQL TASK: Retrieve customer details with abbreviated country code
+SELECT
+CustomerID,
+FirstName,
+LastName,
+Country,
+CASE
+	WHEN Country = 'Germany' THEN 'DE'
+	WHEN Country = 'USA' THEN 'US'
+	ELSE 'Not available'
+END CountryAbbreviation
+FROM Sales.Customers
+-- or using quick form we can write it as: 
+SELECT
+CustomerID,
+FirstName,
+LastName,
+Country,
+CASE Country
+	WHEN 'Germany' THEN 'DE'
+	WHEN 'USA' THEN 'US'
+	ELSE 'Not available'
+END CountryAbbreviation
+FROM Sales.Customers
+
+
+-- 3. HANDLING NULLS: -
+/* SQL TASK: FInd the average score of customers and treat Nulls as '0'
+and provide additional details such as customerID & LastName */
+SELECT
+CustomerID,
+LastName,
+Score,
+CASE
+	WHEN Score IS NULL THEN 0 
+	ELSE Score
+END ScoreClean,
+AVG(CASE
+	WHEN Score IS NULL THEN 0 
+	ELSE Score
+	END ) OVER() AvgCustomerClean,
+
+AVG(Score) OVER() AvgCustomer
+FROM Sales.Customers
+
+
+-- 4. CONDITIONAL AGGREGATION: -
+-- SQL TASK: Count how many times each customer has made an order with sales greater than 30
+SELECT 
+CustomerID,
+SUM(CASE
+	WHEN Sales> 30 THEN 1
+	ELSE 0
+END) TotalOrders,
+COUNT(*) TotalOrders
+FROM Sales.Orders
+GROUP BY CustomerID
