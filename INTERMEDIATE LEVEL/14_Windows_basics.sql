35116251374bc6052cd9eb311cea2fde86eaba42
Shreya-patil04 <patilshreya0405@gmail.com>
2026-01-08 23:53:14 +0530

    Add SQL examples for window functions; include tasks for sales by product and order status

--- INTERMEDIATE LEVEL/14_Windows_basics.sql
+++ INTERMEDIATE LEVEL/14_Windows_basics.sql
@@ -33,20 +33,64 @@ OrderDate,
 SUM(Sales) OVER(PARTITION BY ProductID) TotalSalesByProducts
 FROM Sales.Orders
 
+-- WINDOW SYNTAX: -
 
--- SQL TASK: 
+--1. partition by
+
+-- SQL TASK: Find the total sales across all orders 
+-- additionally provide details such as order ID, order date
+SELECT
+OrderID,
+OrderDate,
+SUM(Sales) OVER() TotalSales
+FROM Sales.Orders
+
+-- SQL TASK: Find the total sales for each product
+-- additionally provide details such as order ID, order date
+SELECT
+OrderID,
+OrderDate,
+ProductID,
+SUM(Sales) OVER(PARTITION BY ProductID) TotalSalesEachProduct
+FROM Sales.Orders
+
+
+/*SQL TASK: Find the total sales across all orders 
+Find the total sales for each product
+additionally provide details such as order ID, order Date */
+SELECT
+OrderID,
+OrderDate,
+ProductID,
+Sales,
+SUM(Sales) OVER() TotalSales,
+SUM(Sales) OVER(PARTITION BY ProductID) TotalSalesEachProduct
+FROM Sales.Orders
+
+/* SQL TASK: Find the total sales across all orders 
+Find the total sales for each product
+Find the total sales for each combination of product and order status 
+additionally provide details such as order ID, order Date */
+SELECT
+OrderID,
+OrderDate,
+ProductID,
+OrderStatus,
+Sales,
+SUM(Sales) OVER() TotalSales,
+SUM(Sales) OVER(PARTITION BY ProductID) TotalSalesEachProduct,
+SUM(Sales) OVER(PARTITION BY ProductID, OrderStatus ) SalesByProductAndStatus
+FROM Sales.Orders
+
+
+-- 2. order by 
+
+-- SQL TASK: rank each order based on their sales from highest to lowest
+-- additionally provide details such as order ID, order Date */
 
 
 
--- SQL TASK: 
 
-
-
--- SQL TASK: 
-
-
-
--- SQL TASK: 
 -- SQL TASK: 
 -- SQL TASK: 
 -- SQL TASK: 
