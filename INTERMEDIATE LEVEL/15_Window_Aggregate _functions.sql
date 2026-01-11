dd18f021ee23a2f205913e0df3cdaed8a1732bde
Shreya-patil04 <patilshreya0405@gmail.com>
2026-01-11 23:16:47 +0530

    Add SQL examples for window functions; include calculations for average sales across all orders and by product, along with average customer scores

--- INTERMEDIATE LEVEL/15_Window_Aggregate _functions.sql
+++ INTERMEDIATE LEVEL/15_Window_Aggregate _functions.sql
@@ -63,3 +63,31 @@ SELECT
     ROUND(CAST(Sales AS FLOAT) / SUM(Sales) OVER () * 100, 2) AS PercentageOfTotal
 FROM Sales.Orders
 
+/* ============================================================
+   SQL WINDOW AGGREGATION | AVG
+   ============================================================ */
+
+/* TASK 6:
+   - Find the Average Sales Across All Orders 
+   - Find the Average Sales for Each Product
+*/
+SELECT
+    OrderID,
+    OrderDate,
+    Sales,
+    ProductID,
+    AVG(Sales) OVER () AS AvgSales,
+    AVG(Sales) OVER (PARTITION BY ProductID) AS AvgSalesByProduct
+FROM Sales.Orders
+
+/* TASK 7:
+   Find the Average Scores of Customers
+*/
+SELECT
+    CustomerID,
+    LastName,
+    Score,
+    COALESCE(Score, 0) AS CustomerScore,
+    AVG(Score) OVER () AS AvgScore,
+    AVG(COALESCE(Score, 0)) OVER () AS AvgScoreWithoutNull
+FROM Sales.Customers
